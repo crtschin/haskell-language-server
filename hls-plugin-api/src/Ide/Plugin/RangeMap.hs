dbf933a4c47656d7fafb36988d2df40e1eeeee0b
Curtis Chin Jen Sem <csochinjensem@gmail.com>
2026-04-19 18:36:23 +0200

    Remove dependency on hw-fingertree
    The rest of the codebase already uses the more actively maintained
    `fingertree`.

--- hls-plugin-api/src/Ide/Plugin/RangeMap.hs
+++ hls-plugin-api/src/Ide/Plugin/RangeMap.hs
@@ -1,13 +1,9 @@
-{-# LANGUAGE CPP                #-}
 {-# LANGUAGE DerivingStrategies #-}
 
 -- | A map that allows fast \"in-range\" filtering. 'RangeMap' is meant
 -- to be constructed once and cached as part of a Shake rule. If
 -- not, the map will be rebuilt upon each invocation, yielding slower
 -- results compared to the list-based approach!
---
--- Note that 'RangeMap' falls back to the list-based approach if
--- `use-fingertree` flag of `hls-plugin-api` is set to false.
 module Ide.Plugin.RangeMap
   ( RangeMap(..),
     fromList,
@@ -16,67 +12,37 @@ module Ide.Plugin.RangeMap
     elementsInRange,
   ) where
 
-import           Development.IDE.Graph.Classes            (NFData)
-
-#ifdef USE_FINGERTREE
-import           Data.Bifunctor                           (first)
-import qualified HaskellWorks.Data.IntervalMap.FingerTree as IM
-import           Language.LSP.Protocol.Types              (Position,
-                                                           Range (Range))
-#else
-import           Language.LSP.Protocol.Types              (Range, isSubrangeOf)
-#endif
-
-#if USE_FINGERTREE && !MIN_VERSION_base(4,20,0)
-import           Data.List                                (foldl')
-#endif
+import           Data.Bifunctor                (first)
+import qualified Data.IntervalMap.FingerTree   as IM
+import           Development.IDE.Graph.Classes (NFData)
+import           Language.LSP.Protocol.Types   (Position, Range (Range))
 
 -- | A map from code ranges to values.
-#ifdef USE_FINGERTREE
 newtype RangeMap a = RangeMap
   { unRangeMap :: IM.IntervalMap Position a
     -- ^ 'IM.Interval' of 'Position' corresponds to a 'Range'
   }
   deriving newtype (NFData, Semigroup, Monoid)
   deriving stock (Functor, Foldable, Traversable)
-#else
-newtype RangeMap a = RangeMap
-  { unRangeMap :: [(Range, a)] }
-  deriving newtype (NFData, Semigroup, Monoid)
-  deriving stock (Functor, Foldable, Traversable)
-#endif
 
 -- | Construct a 'RangeMap' from a 'Range' accessor and a list of values.
 fromList :: (a -> Range) -> [a] -> RangeMap a
 fromList extractRange = fromList' . map (\x -> (extractRange x, x))
 
 fromList' :: [(Range, a)] -> RangeMap a
-#ifdef USE_FINGERTREE
 fromList' = RangeMap . toIntervalMap . map (first rangeToInterval)
   where
     toIntervalMap :: Ord v => [(IM.Interval v, a)] -> IM.IntervalMap v a
     toIntervalMap = foldl' (\m (i, v) -> IM.insert i v m) IM.empty
-#else
-fromList' = RangeMap
-#endif
 
 -- | Filter a 'RangeMap' by a given 'Range'.
 filterByRange :: Range -> RangeMap a -> [a]
-#ifdef USE_FINGERTREE
 filterByRange range = map snd . IM.dominators (rangeToInterval range) . unRangeMap
-#else
-filterByRange range = map snd . filter (isSubrangeOf range . fst) . unRangeMap
-#endif
 
 -- | Extracts all elements from a 'RangeMap' that fall within a given 'Range'.
 elementsInRange :: Range -> RangeMap a -> [a]
-#ifdef USE_FINGERTREE
 elementsInRange range = map snd . IM.intersections (rangeToInterval range) . unRangeMap
-#else
-elementsInRange range = map snd . filter (flip isSubrangeOf range . fst) . unRangeMap
-#endif
 
-#ifdef USE_FINGERTREE
 -- NOTE(ozkutuk): In itself, this conversion is wrong. As Michael put it:
 -- "LSP Ranges have exclusive upper bounds, whereas the intervals here are
 -- supposed to be closed (i.e. inclusive at both ends)"
@@ -90,4 +56,3 @@ elementsInRange range = map snd . filter (flip isSubrangeOf range . fst) . unRan
 -- worry about other uses where it potentially makes a difference.
 rangeToInterval :: Range -> IM.Interval Position
 rangeToInterval (Range s e) = IM.Interval s e
-#endif
